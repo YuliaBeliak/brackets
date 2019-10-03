563053a79f6c595aa86aab03f0c195a31f55b049
YuliaBeliak <belyakzhe@gmail.com>
2019-10-03 14:22:17 +0300

    feat: implement check fn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+    let confStr = bracketsConfig.map(e => e.join());
+    let stack = [];
+    stack.push(str[0]);
+    for (let i = 1; i < str.length; i++) {
+        let check = `${stack[stack.length - 1]},${str[i]}`;
+        if (!stack.length || confStr.indexOf(check) === -1) {
+            stack.push(str[i]);
+        } else {
+            stack.pop();
+        }
+    }
+    return !stack.length;
+};
